d6aa9a50eed7296f28279c4de7986cb24fee518b
Frode Nordahl <frode.nordahl@canonical.com>
2019-06-04 11:58:29 +0200

    Updates
    libguestfs appliance: Create /etc/passwd and /etc/group with root
    user to accomodate elements executing outside /sysroot that want
    to chown files.
    
    libguestfs dib: Allow running ``root.d`` elements in the existing
    image.
    TODO: Disallow running of elements that provde operating-sytem
    
    elements: Add ``debian-networking`` element to be compatible with
    images created with the ``debootstrap`` element.
    
    elements: Add documentation for ``ubuntu-cloud-archive`` element
    
    retrofit: Improvements, run virt-sparsify after modification is
    done and before converting out the temp image.

---
+++ src/elements/ubuntu-cloud-archive/README.rst
@@ -0,0 +1,7 @@
+====================
+ubuntu-cloud-archive
+====================
+
+Add the Ubuntu Cloud Archive (UCA) repository to the image.
+
+Set ``DIB_UBUNTU_CLOUD_ARCHIVE`` to the pocket you want.
--- src/retrofit/retrofit.sh
+++ src/retrofit/retrofit.sh
@@ -25,10 +25,11 @@ DIB_UBUNTU_CLOUD_ARCHIVE=${DIB_UBUNTU_CLOUD_ARCHIVE:-rocky}
 TEMP_IMAGE_FILE=$(mktemp $SNAP_COMMON/tmp/output-XXXXXX.raw)
 TEMP_IMAGE_NAME=$(echo ${TEMP_IMAGE_FILE}|cut -f1 -d\.)
 
-qemu-img convert $INPUT_IMAGE $TEMP_IMAGE_FILE
+qemu-img convert -O raw $INPUT_IMAGE $TEMP_IMAGE_FILE
 
 cd $SNAP_COMMON/tmp
-virt-dib -vvv -xxxx -B /snap/octavia-diskimage-retrofit/current/lib/python3.6/site-packages/diskimage_builder/lib \
+virt-dib -vvv -xxxx \
+	 -B /snap/octavia-diskimage-retrofit/current/lib/python3.6/site-packages/diskimage_builder/lib \
          -p /snap/octavia-diskimage-retrofit/current/lib/python3.6/site-packages/diskimage_builder/elements \
 	 -p /snap/octavia-diskimage-retrofit/current/usr/local/lib/elements \
 	 --formats raw \
@@ -41,6 +42,9 @@ virt-dib -vvv -xxxx -B /snap/octavia-diskimage-retrofit/current/lib/python3.6/si
 	 --python /snap/octavia-diskimage-retrofit/current/usr/bin/python3 \
 	 --install-type package \
 	 --extra-packages initramfs-tools \
-	 ubuntu-minimal ubuntu-cloud-archive haproxy-octavia rebind-sshd no-resolvconf amphora-agent sos keepalived-octavia ipvsadmin pip-cache certs-ramfs
+	 dpkg debian-networking ubuntu-cloud-archive \
+	 haproxy-octavia rebind-sshd no-resolvconf amphora-agent \
+	 sos keepalived-octavia ipvsadmin pip-cache certs-ramfs
 
-qemu-img convert $TEMP_IMAGE_FILE $OUTPUT_IMAGE
+virt-sparsify --in-place $TEMP_IMAGE_FILE
+qemu-img convert -O qcow2 $TEMP_IMAGE_FILE $OUTPUT_IMAGE
