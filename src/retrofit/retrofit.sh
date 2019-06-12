2270a3c87ab01ba6415721388bfc3cfac62515b9
Frode Nordahl <frode.nordahl@canonical.com>
2019-06-12 20:11:50 +0200

    retrofit: Clean up CLI
    Translate relative paths into real paths.
    
    Validate placement of input/output files.
    
    Add command line argument for debug messages (disabled by default).
    
    Add command line argument for output format (defaults to qcow2).
    
    Remove options to ``virt-dib`` used during developemnt.
    
    Copy manifest to directory of output file.
    
    Remove temporary files.
    
    Suppress warning message from ``virt-sparsify``.
    
    Clean up use of some environment variables.

--- src/retrofit/retrofit.sh
+++ src/retrofit/retrofit.sh
@@ -3,39 +3,59 @@
 NAME=$(basename $0)
 
 usage() {
-    echo $NAME: input_image_file.format output_image_file.format [-h] \
-        [-u Ubuntu Cloud Archive pocket]
-    exit 1
+    echo $NAME: input output [-dh] \
+        [-u Ubuntu Cloud Archive pocket] [-O output format}
+    exit 64
 }
 
-while getopts "hu:" options; do
+while getopts "dhu:O:" options; do
     case "${options}" in
+        d)
+            DEBUG="-v -xxxx"
+            ;;
         h)
             usage
             ;;
         u)
             DIB_UBUNTU_CLOUD_ARCHIVE=$OPTARG
             ;;
+        O)
+            OUTPUT_FORMAT=$OPTARG
+            ;;
     esac
 done
 shift $(($OPTIND - 1))
-INPUT_IMAGE=$1
-OUTPUT_IMAGE=$2
-DIB_UBUNTU_CLOUD_ARCHIVE=${DIB_UBUNTU_CLOUD_ARCHIVE:-rocky}
 
-TEMP_IMAGE_FILE=$(mktemp $SNAP_COMMON/tmp/output-XXXXXX.raw)
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+INPUT_IMAGE=$(realpath $1)
+OUTPUT_IMAGE=$(realpath $2)
+if ! [[ "$INPUT_IMAGE" =~ ^${SNAP_COMMON}/.* && \
+        "$OUTPUT_IMAGE" =~ ^${SNAP_COMMON}/.* ]]; then
+    echo "$NAME: both input and output image must reside within '$SNAP_COMMON'"
+    exit 65
+fi
+
+# Set defaults
+DIB_UBUNTU_CLOUD_ARCHIVE=${DIB_UBUNTU_CLOUD_ARCHIVE:-rocky}
+OUTPUT_FORMAT=${OUTPUT_FORMAT:-qcow2}
+if [ $OUTPUT_FORMAT == "qcow2" ]; then
+    COMPRESS="-c"
+fi
+
+TEMP_IMAGE_FILE=$(mktemp $TMPDIR/output-XXXXXX.raw)
 TEMP_IMAGE_NAME=$(echo ${TEMP_IMAGE_FILE}|cut -f1 -d\.)
 
 qemu-img convert -O raw $INPUT_IMAGE $TEMP_IMAGE_FILE
 
-cd $SNAP_COMMON/tmp
-virt-dib -vvv -xxxx \
+virt-dib ${DEBUG} \
     -B $SNAP/lib/python3.6/site-packages/diskimage_builder/lib \
     -p $SNAP/lib/python3.6/site-packages/diskimage_builder/elements \
     -p $SNAP/usr/local/lib/elements \
     --formats raw \
     --name $TEMP_IMAGE_NAME \
-    --no-delete-on-failure \
     --envvar DISTRO_NAME=ubuntu \
     --envvar DIB_RELEASE=bionic \
     --envvar DIB_PYTHON_VERSION=3 \
@@ -53,5 +73,11 @@ virt-sysprep -a $TEMP_IMAGE_FILE \
     --delete "/var/lib/apt/*" \
     --delete "/var/cache/*"
 
-virt-sparsify --in-place $TEMP_IMAGE_FILE
-qemu-img convert -c -O qcow2 $TEMP_IMAGE_FILE $OUTPUT_IMAGE
+virt-sparsify --ignore /dev/sda15 --in-place $TEMP_IMAGE_FILE
+
+qemu-img convert ${COMPRESS} -O $OUTPUT_FORMAT $TEMP_IMAGE_FILE $OUTPUT_IMAGE
+
+cp ${TEMP_IMAGE_NAME}.d/dib-manifests/dib-manifest-dpkg-$(basename ${TEMP_IMAGE_NAME}) \
+    ${OUTPUT_IMAGE}.manifest
+
+rm -rf $TEMP_IMAGE_FILE ${TEMP_IMAGE_NAME}.d
