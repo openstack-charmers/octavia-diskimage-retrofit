4bc9d0ea2dab2c6c7b1903c812c3d663859985d2
Frode Nordahl <frode.nordahl@canonical.com>
2019-06-12 07:59:02 +0200

    retrofit: Fix cache removal
    The ``customize`` operation must be added to the ``virt-sysprep``
    command line for the `--delete`` command line arguments to have
    effect.

--- src/retrofit/retrofit.sh
+++ src/retrofit/retrofit.sh
@@ -48,6 +48,10 @@ virt-dib -vvv -xxxx \
     sos keepalived-octavia ipvsadmin pip-cache certs-ramfs \
     ubuntu-amphora-agent
 
-virt-sysprep -a $TEMP_IMAGE_FILE --operations tmp-files --delete "/var/lib/apt/*" --delete "/var/cache/*"
+virt-sysprep -a $TEMP_IMAGE_FILE \
+    --operations tmp-files,customize \
+    --delete "/var/lib/apt/*" \
+    --delete "/var/cache/*"
+
 virt-sparsify --in-place $TEMP_IMAGE_FILE
 qemu-img convert -c -O qcow2 $TEMP_IMAGE_FILE $OUTPUT_IMAGE
